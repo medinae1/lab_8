3d345dc7bb0df84aa9bc057f134d7500038a5786
medinae1 <117927715+medinae1@users.noreply.github.com>
2024-03-17 23:43:16 +0000

    test

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.7.1"
 const supabaseUrl = 'https://lzjxptrsfdpkxfmqvldt.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6anhwdHJzZmRwa3hmbXF2bGR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3MTExODAsImV4cCI6MjAyNjI4NzE4MH0.Ck2lArIxtuaQX6lchq1z6RBaWaNQ77W1EZT4PthX7b4'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-async function getBooks(){
+async function getBooks() {
     let { data: books, error } = await supabase
-    .from('books')
-    .select('*')
-    for (let book of books){
-      let bookList = document.getElementById('books');
-      bookList.innerHTML +=  `<li>${book.title} - ${book.author} -${book.isbn}</li>`;
-
-    }
-}
-getBooks();
+      .from('books')
+      .select('*')
+  
+    for (let book of books) {
+       let bookList = document.getElementById('books');
+       bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.publisher}</td></tr>`;
+     }
+  }
+  
+  getBooks();
